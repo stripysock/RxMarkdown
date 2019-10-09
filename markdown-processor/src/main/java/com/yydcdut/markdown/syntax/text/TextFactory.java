4725a9ffb77e8cd11f8291bfe1f138d571d076a1
Ben McLean <ben@origma.com.au>
2019-10-09 19:37:28 +1100

    Made TextFactory constructor public

--- markdown-processor/src/main/java/com/yydcdut/markdown/syntax/text/TextFactory.java
+++ markdown-processor/src/main/java/com/yydcdut/markdown/syntax/text/TextFactory.java
@@ -42,7 +42,7 @@ public class TextFactory implements SyntaxFactory {
     private ISpecialChain mLineChain;
     private ISpecialChain mTotalChain;
 
-    private TextFactory() {
+    public TextFactory() {
     }
 
     /**
